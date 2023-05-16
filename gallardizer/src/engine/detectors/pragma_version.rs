f38436b317fe40fff1d7000d31a08a3812ccc06b
Lior <97247251+lior-abadi@users.noreply.github.com>
2023-05-16 00:11:14 -0300

    better: pragma_version format

--- gallardizer/src/engine/detectors/pragma_version.rs
+++ gallardizer/src/engine/detectors/pragma_version.rs
@@ -45,15 +45,19 @@ impl Detector for PragmaVersionDetector {
         let metadata: IssueMetadata = IssueMetadata {
             severity: Severities::L,
             title: r#"Inconsistent floating pragma version"#.to_string(),
-            content: indoc! {"The specified `pragma` version allows for the utilization of compiler versions beyond 0.8.0 to compile the source code.
+            content: indoc! {
+            "The specified <code>pragma</code> version allows for the utilization of compiler versions
+            beyond <code>0.8.0</code> to compile the source code.
             However, it's important to consider the potential risks associated with using a floating pragma version.<br>
 
-            Employing versions 0.8.7 or earlier may result in compilation errors, as they lack support for functions overriding interface 
-            functions without using the 'override' keyword, which is exclusively available in Solidity 0.8.8 and newer versions. 
-            Similarly, the usage of abi.encodeCall, which was introduced in Solidity 0.8.11, may cause issues if the codebase relies on it.<br>
+            Employing versions <code>0.8.7</code>or earlier may result in compilation errors, as they lack support for
+            functions overriding interface functions without using the <code>override</code> modifier, which is exclusively 
+            available in Solidity <code>0.8.8</code> and newer versions. Similarly, the usage of abi.encodeCall, 
+            which was introduced in Solidity <code>0.8.11</code>, may cause issues if the codebase relies on it.<br>
 
-            While it is not confirmed whether these specific bugs related to override or encoding will appear in the code, it is advised to be cautious. 
-            Considering the uncertainty of potential bugs related to override, encoding, or others, it is recommended to avoid using a floating pragma version.<br>
+            While it is not confirmed whether these specific bugs related to override or encoding will appear in the code, 
+            it is advised to be cautious. Considering the uncertainty of potential bugs related to 
+            <code>override</code>, <code>encode</code>, or others, it is recommended to avoid using a floating pragma version.<br>
 
             Consider upgrading the pragma version to a newer release the most recent version available, 
             in order to mitigate potential risks leveraging from bug fixes introduced on newer releases. 
