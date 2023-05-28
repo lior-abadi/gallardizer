020c806ca3cc2fd785d00e51dd3098c6c4f435f0
Lior <97247251+lior-abadi@users.noreply.github.com>
2023-05-27 21:22:18 -0300

    add: numeric time variables detector

--- gallardizer/src/engine/detectors/NonCritical.rs
+++ gallardizer/src/engine/detectors/NonCritical.rs
@@ -1,5 +1,6 @@
 pub mod magic_numbers;
 pub mod missing_indexed_fields;
+pub mod numeric_timevariables;
 pub mod reentrancy_modifier_precedence;
 pub mod revert_strings;
 pub mod scientific_notation;
